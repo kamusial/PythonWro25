b2d293e3e9d574638546e0427ae0d78ea93e125f
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-01-19 14:36:45 +0100

    '1'

--- 12_w3school.py
+++ 12_w3school.py
@@ -19,4 +19,25 @@ menu = driver.find_element('id', 'navbtn_tutorials')
 HTMLtutorial = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/nav[1]/div[1]/div/div[2]/div[1]/div[1]/a[2]')
 webdriver.ActionChains(driver).move_to_element(menu).click().move_to_element(HTMLtutorial).click().perform()
 sleep(3)
+driver.find_element(By.ID, 'accept-choices').click()
+HTML_forms_attributes = driver.find_element(By.XPATH, '//*[@id="leftmenuinnerinner"]/a[41]')
+HTML_forms_attributes.click()
+tryityourself = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/a')
+tryityourself.click()
+sleep(3)
+
+currentWindowName = driver.current_window_handle
+windowNames = driver.window_handles
+print(currentWindowName)
+print(windowNames)
+
+for window in windowNames:
+    if window != currentWindowName:
+        driver.switch_to.window(window)
+
+driver.switch_to.frame(driver.find_element(By.ID,'iframeResult'))
+firstName = driver.find_element(By.ID, 'fname')
+firstName.send_keys('Kamil')
+sleep(2)
+
 driver.quit()
