c2a777b3cc5f5daaa9cd16644144d497241916a4
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-01-19 14:42:46 +0100

    '1'

--- 12_w3school.py
+++ 12_w3school.py
@@ -18,13 +18,13 @@ sleep(1)
 menu = driver.find_element('id', 'navbtn_tutorials')
 HTMLtutorial = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/nav[1]/div[1]/div/div[2]/div[1]/div[1]/a[2]')
 webdriver.ActionChains(driver).move_to_element(menu).click().move_to_element(HTMLtutorial).click().perform()
-sleep(3)
+sleep(1)
 driver.find_element(By.ID, 'accept-choices').click()
 HTML_forms_attributes = driver.find_element(By.XPATH, '//*[@id="leftmenuinnerinner"]/a[41]')
 HTML_forms_attributes.click()
 tryityourself = driver.find_element(By.XPATH, '//*[@id="main"]/div[3]/a')
 tryityourself.click()
-sleep(3)
+sleep(1)
 
 currentWindowName = driver.current_window_handle
 windowNames = driver.window_handles
@@ -37,7 +37,11 @@ for window in windowNames:
 
 driver.switch_to.frame(driver.find_element(By.ID,'iframeResult'))
 firstName = driver.find_element(By.ID, 'fname')
-firstName.send_keys('Kamil')
-sleep(2)
+if firstName.is_enabled():
+    firstName.clear()
+    firstName.send_keys('Kamil')
+else:
+    print('Nie da się wpisać')
+sleep(3)
 
 driver.quit()
