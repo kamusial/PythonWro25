b63f6a04cfa1ea76ca62089aca720f88837aac72
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-02-01 14:31:04 +0100

    1

---
+++ ML/4_kolka.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_circles
+from sklearn.svm import SVC  #clasifiaer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+X, y = make_circles(n_samples=500, factor=0.4, noise=0.3)
+print(X)
+print(y)
+plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.show()
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+# kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}
+
+
+print('\nLogistic Regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nKNN')
+model = KNeighborsClassifier(n_neighbors=5) #, weights='distance')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nDrzewo decyzyjne')
+model1 = DecisionTreeClassifier(max_depth=30, min_samples_split=2)
+model1.fit(X_train, y_train)
+print(model1.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model1.predict(X_test))))
+
+print('\nSVC')
+model = SVC()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+# # granice decyzyjne
+from mlxtend.plotting import plot_decision_regions
+plot_decision_regions(X, y, model1)
+plt.show()
