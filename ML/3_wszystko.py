0bcc8699ecd2ade67a38723006fc42c9c4084bbc
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-02-01 13:58:33 +0100

    1

--- ML/3_wszystko.py
+++ ML/3_wszystko.py
@@ -1,9 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_circles
 from sklearn.svm import SVC  #clasifiaer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv('dane\\heart.csv', comment='#')
+print(df)
+
+X = df.iloc[:, :-1]   #wszystkie kolumny, bez ostatniej
+y = df.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+print('\nLogistic Regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nKNN')
+model = KNeighborsClassifier(n_neighbors=5) #, weights='distance')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nDrzewo decyzyjne')
+model1 = DecisionTreeClassifier(max_depth=30, min_samples_split=2)
+model1.fit(X_train, y_train)
+print(model1.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model1.predict(X_test))))
+
+print('\nSVC')
+model = SVC()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
