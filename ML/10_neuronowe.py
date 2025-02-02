f6163978ffe01c2065fc80f5baa0d2a4b2162cda
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-02-02 14:00:52 +0100

    '1'

---
+++ ML/10_neuronowe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
