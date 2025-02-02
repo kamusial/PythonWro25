ecac43957fe87b9db7515f56414a96537f746076
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-02-02 14:48:02 +0100

    '1'

--- ML/10_neuronowe.py
+++ ML/10_neuronowe.py
@@ -7,3 +7,24 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 model = Sequential()
+model.add(Dense(1, activation='linear'))
+model.add(Dense(4, activation='linear'))
+model.add(Dense(4, activation='linear'))
+model.add(Dense(1, activation='linear'))
+
+model.compile(optimizer='rmsprop', loss='mse')
+
+df = pd.read_csv('f-c.csv', usecols=[1, 2])
+print(df)
+
+
+result = model.fit(df.F, df.C, epochs=10, verbose=2)
+df1 = pd.DataFrame(result.history)
+print(df1)
+df1.plot()
+plt.show()
+
+C_pred = model.predict(df.F)
+plt.scatter(df.F, df.C)
+plt.plot(df.F, C_pred, c='r')
+plt.show()
