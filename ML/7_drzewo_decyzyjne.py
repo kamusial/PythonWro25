9fd9467fad1c5f4321d7a81cf41dbde5a89d1407
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-02-02 13:53:45 +0100

    '1'

--- ML/7_drzewo_decyzyjne.py
+++ ML/7_drzewo_decyzyjne.py
@@ -31,12 +31,13 @@ y = df.class_value
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 
-model = DecisionTreeClassifier()
+model = DecisionTreeClassifier(max_depth=20, min_samples_split=2)
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+print(pd.DataFrame(model.feature_importances_, X.columns))
 
 # granice decyzyjne
-from mlxtend.plotting import plot_decision_regions
-plot_decision_regions(X.values, y.values, model)
-plt.show()
+# from mlxtend.plotting import plot_decision_regions
+# plot_decision_regions(X.values, y.values, model)
+# plt.show()
