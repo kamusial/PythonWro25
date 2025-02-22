24d082307a6b763c6ad591b9ac057bec8e4704ea
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-02-22 13:17:40 +0100

    '1'

--- UX/10_program.py
+++ UX/10_program.py
@@ -12,20 +12,28 @@ textbox = tk.Text(root, height=3, font=('Arial', 14))
 textbox.pack(padx=10, pady=10)
 buttonframe = tk.Frame(root)
 buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
+buttonframe.columnconfigure(1, weight=3)
 buttonframe.columnconfigure(2, weight=1)
-# buttonframe.rowconfigure
+# buttonframe.rowconfigure(0, weight=3)
+# buttonframe.rowconfigure(1, weight=1)
 
 btn1 = tk.Button(buttonframe, text='1', font=('Arial', 15))
-btn1.grid(row=0, column=0)
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E, pady=10, padx=10)
 btn2 = tk.Button(buttonframe, text='2', font=('Arial', 15))
-btn2.grid(row=0, column=1)
+btn2.grid(row=0, column=1, sticky=tk.W+tk.E, pady=10, padx=10)
 btn3 = tk.Button(buttonframe, text='3', font=('Arial', 15))
-btn3.grid(row=0, column=2)
+btn3.grid(row=0, column=2, sticky=tk.W+tk.E, pady=10, padx=10)
+btn4 = tk.Button(buttonframe, text='4', font=('Arial', 15))
+btn4.grid(row=1, column=0, sticky=tk.W+tk.E, pady=10, padx=10)
+btn5 = tk.Button(buttonframe, text='5', font=('Arial', 15))
+btn5.grid(row=1, column=1, sticky=tk.W+tk.E, pady=10, padx=10)
+btn6 = tk.Button(buttonframe, text='6', font=('Arial', 15))
+btn6.grid(row=1, column=2, sticky=tk.W+tk.E, pady=10, padx=10)
 
+buttonframe.pack(fill='x')
 
-buttonframe.pack()
-
+quit_btn = tk.Button(root, text='Wyjdz', command=root.quit, bg='red')
+quit_btn.
 
 
 
